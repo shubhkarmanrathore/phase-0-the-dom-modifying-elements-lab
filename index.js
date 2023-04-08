dc5611856c07c783335892c9e9cd9eaddf9d793f
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-04-08 16:47:17 -0700

    adds dom nodes lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Shubh is the champion';
